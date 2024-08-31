56f875cb29c554e53ae0741c702a0c49bc6173fd
Dhvanibarot5 <dhvanibarot5@gmail.com>
2024-09-01 02:42:56 +0530

    add js and new section

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ items.forEach((item, index) => {
     wrapper.style.transform = `translateX(${-100 * index}vw)`;
   });
 });
+
+let move = 0;
+setInterval(() => {
+  if (move == (items.length - 1) * 100) {
+    move = 0;
+    wrapper.style.transform = `translateX(${-1 * move}vw)`;
+  } else {
+    move += 100;
+    wrapper.style.transform = `translateX(${-1 * move}vw)`;
+  }
+}, 3000);
